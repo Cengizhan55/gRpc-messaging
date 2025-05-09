878be4c8bac8fabca02d8cb488c62dffc73150de
Cengizhan55 <cengiz_er_@hotmail.com>
2025-05-10 00:01:08 +0300

    try

--- GrpcClient/src/main/java/com/cengizhaner/GrpcClient/GrpcClientApplication.java
+++ GrpcClient/src/main/java/com/cengizhaner/GrpcClient/GrpcClientApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrpcClientApplication {
 
-	// main
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcClientApplication.class, args);
 	}
